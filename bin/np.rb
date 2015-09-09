d410e964545270281687125efcf238d4c09eaa8d
edhowland <ed.howland@gmail.com>
2015-09-09 23:11:27 +0000

    \'moved bin/chain.rb to examples/\'

--- bin/np.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH.unshift '../lib'
-require 'proc_chain'
-
-
-p = ->(x){ puts x }
-p.say
-
--- bin/tp.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift '../lib'
-require 'proc_chain'
-
-using ProcChain
-
-p = ->(x){ puts x }
-p p.respond_to? :say
-p.say
-
