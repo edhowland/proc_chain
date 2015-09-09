5ee1037aae6067797e21b7ad5e6684c474a1bfd7
edhowland <ed.howland@gmail.com>
2015-09-09 23:36:32 +0000

    \'working algabraic tree\'

--- examples/rpn_calc.rb
+++ examples/rpn_calc.rb
@@ -2,10 +2,14 @@
 # rpn_calc.rb - RPN style calculator
 
 require_relative '../lib/proc_chain'
-
+# 5 4 * 8 2 + -
 a=[->(x){ x.push 5},
 ->(x){ x.push 4},
-->(x){ x.push(x.pop * x.pop)}
+->(x){ x.push(x.pop * x.pop)},
+->(x){ x.push 8},
+->(x){ x.push 2},
+->(x){ x.push(x.pop + x.pop)},
+->(x){ x.push(x.pop - x.pop)}
 ]
 
 
