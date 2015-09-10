6d194379eb0ec597508d1346b4dea5645e7c2eb3
edhowland <ed.howland@gmail.com>
2015-09-10 01:47:40 +0000

    \'refactored everything into a method : rpn_calc\'

--- examples/rpn_calc.rb
+++ examples/rpn_calc.rb
@@ -5,41 +5,43 @@ require_relative '../lib/proc_chain'
 
 # (5 * 4) - (8 + 2)
 strng =   '8 2 + 5 4 * -'
-rpn = strng.split.map do |e|
-  if e =~ /[\+\-\*\/]/
-    e.to_sym
-  else
-    e.to_i
+def rpn_calc(strng)
+  rpn = strng.split.map do |e|
+    if e =~ /[\+\-\*\/]/
+      e.to_sym
+    else
+      e.to_i
+    end
   end
-end
 
-puts 'intermediate:'
-p rpn
-a= rpn.map do |e|
-  case e
-    when Integer
-      ->(x){ x.push e}
-    when :+
-      ->(x){ x.push(x.pop + x.pop)}
+  puts 'intermediate:'
+  p rpn
+  a= rpn.map do |e|
+    case e
+      when Integer
+        ->(x){ x.push e}
+      when :+
+        ->(x){ x.push(x.pop + x.pop)}
 
-    when :-
-      ->(x){ x.push(x.pop - x.pop)}
+      when :-
+        ->(x){ x.push(x.pop - x.pop)}
 
-    when :*
-      ->(x){ x.push(x.pop * x.pop)}
+      when :*
+        ->(x){ x.push(x.pop * x.pop)}
 
-    when :/
-      ->(x){ x.push(x.pop / x.pop)}
+      when :/
+        ->(x){ x.push(x.pop / x.pop)}
+    end
   end
+
+
+
+
+
+  chain = ProcChainer.new(->(x){x})
+  chain.lambdas =  a
+
+  result = chain.call([])
+  puts result.pop
+
 end
-
-
-
-
-
-chain = ProcChainer.new(->(x){x})
-chain.lambdas =  a
-
-result = chain.call([])
-puts result.pop
-
